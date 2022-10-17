6aef16f6c315835dbffbc2f6833b0e5a39a7efb0
minchodang <minsu910725@gmail.com>
2022-10-17 20:58:09 +0900

    Feat: api Route 요청, APi Get data, 사전 랜더링 페이지

--- pages/api/feedback.ts
+++ pages/api/feedback.ts
@@ -3,10 +3,20 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import fs from 'fs';
 import path from 'path';
 
-type Data = {
-    message: string;
+export type Data = {
+    message?: string;
     feedback?: { id: string; email: string; text: string };
 };
+
+export function buildFeedbackPath() {
+    return path.join(process.cwd(), 'data', 'feedback.json');
+}
+
+export function extractFeedback(filePath: fs.PathOrFileDescriptor) {
+    const fileData = fs.readFileSync(filePath);
+    const data = JSON.parse(String(fileData));
+    return data;
+}
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
     if (req.method === 'POST') {
@@ -17,9 +27,9 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
             email: email,
             text: feedbackText,
         };
-        const filePath = path.join(process.cwd(), 'data', 'feedback.json');
-        const fileData = fs.readFileSync(filePath);
-        const data = JSON.parse(fileData.toString());
+        const filePath = buildFeedbackPath();
+        const data = extractFeedback(filePath);
+
         data.push(newFeedback);
         fs.writeFileSync(filePath, JSON.stringify(data));
         res.status(201).json({
@@ -27,6 +37,8 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
             feedback: newFeedback,
         });
     } else {
-        res.status(200).json({ message: 'This works!' });
+        const filePath = buildFeedbackPath();
+        const data = extractFeedback(filePath);
+        res.status(200).json({ feedback: data });
     }
 }
