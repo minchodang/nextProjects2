7b6b3f9f10947c59a5d46b441ff26505aef4047b
minsuKang <90169703+minchodang@users.noreply.github.com>
2022-10-20 23:49:26 +0900

    Feat: dynamicApiRoutes 추가 (#7)

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,24 +5,23 @@ var fs = require('fs');
 var path = require('path');
 
 var parserOptions = {
-project: './tsconfig.json'
+    project: './tsconfig.json',
 };
 
 if (!fs.existsSync(path.join(process.env.PWD || '.', './tsconfig.json'))) {
-parserOptions = {
-tsconfigRootDir: __dirname,
-project: './tsconfig.json'
-};
+    parserOptions = {
+        tsconfigRootDir: __dirname,
+        project: './tsconfig.json',
+    };
 }
 
 module.exports = {
-root: true,
-parser: '@typescript-eslint/parser',
-plugins: ['react-hooks'],
-rules: {
-'react-hooks/rules-of-hooks': 'error',
-'react-hooks/exhaustive-deps': 'error'
-},
-parserOptions: parserOptions,
-// extends: ['plugin:storybook/recommended']
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: ['react-hooks'],
+    rules: {
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'error',
+    },
+    parserOptions: parserOptions,
 };
--- pages/feedback/index.tsx
+++ pages/feedback/index.tsx
@@ -1,13 +1,35 @@
 import { buildFeedbackPath, Data, extractFeedback } from '../api/feedback';
 import { InferGetStaticPropsType } from 'next';
+import axios from 'axios';
+import { useState } from 'react';
+
+interface FeedbackResponseType {
+    id: string;
+    email: string;
+    text: string;
+}
 
 const FeedbackPage = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
+    const [feedbackData, setFeedbackData] = useState<FeedbackResponseType>();
+    const loadFeedbackHandler = (id: string) => {
+        axios.post(`/api/${id}`).then((response) => setFeedbackData(response.data.feedback));
+    };
+
     return (
-        <ul>
-            {props.feedbackItems.map((item: any) => (
-                <li key={item.id}>{item.text}</li>
-            ))}
-        </ul>
+        <>
+            {feedbackData && <p>{feedbackData.email}</p>}
+
+            <ul>
+                {props.feedbackItems.map((item: any) => (
+                    <li key={item.id}>
+                        {item.text}
+                        <button onClick={loadFeedbackHandler.bind(null, item.id)}>
+                            Show Details
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </>
     );
 };
 
