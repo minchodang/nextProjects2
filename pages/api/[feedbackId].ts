a2d4b77c0d1d02dc616dda95a1930198202543c0
minsuKang <90169703+minchodang@users.noreply.github.com>
2022-10-21 23:18:03 +0900

    Develop (#8)
    * Feat: first apiRoute, front format setting (#1)
    
    * Feat: serverSide 코드, api route request (#3)
    
    * Feat: api Route 요청, APi Get data, 사전 랜더링 페이지 (#5)
    
    * Feat: dynamicApiRoutes 추가 (#7)

---
+++ pages/api/[feedbackId].ts
@@ -0,0 +1,13 @@
+import { buildFeedbackPath, extractFeedback } from './feedback';
+
+const handler = (req: { query: { feedbackId: any } }, res: any) => {
+    const feedbackId = req.query.feedbackId;
+    const filePath = buildFeedbackPath();
+    const feedbackData = extractFeedback(filePath);
+    const selectedFeedback = feedbackData.find(
+        (feedback: { id: any }) => feedback.id === feedbackId,
+    );
+    res.status(200).json({ feedback: selectedFeedback });
+};
+
+export default handler;
